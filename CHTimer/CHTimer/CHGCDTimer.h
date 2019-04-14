85548b85818940895f13f011c20325ad557fef25
chenyuliang <chenyuliang@didichuxing.com>
2019-04-14 22:11:43 +0800

    GCDTimer

---
+++ CHTimer/CHTimer/CHGCDTimer.h
@@ -0,0 +1,17 @@
+//
+//  CHGCDTimer.h
+//  CHTimer
+//
+//  Created by chenyuliang on 2019/4/14.
+//  Copyright © 2019 didi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CHGCDTimer : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
